9c04fa519264401165486821e82e391ba415c111
Ilshat Nurgalimov <il.nur86@mail.ru>
2023-02-02 23:31:08 +0300

    mentor-1: init

---
+++ mentor_db/src/main/java/ru/inur/mentor_db/MentorDbApplication.java
@@ -0,0 +1,13 @@
+package ru.inur.mentor_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentorDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MentorDbApplication.class, args);
+    }
+
+}
---
+++ mentor_db/src/test/java/ru/inur/mentor_db/MentorDbApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.inur.mentor_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class MentorDbApplicationTests {
+
+//    @Test
+    void contextLoads() {
+    }
+
+}
